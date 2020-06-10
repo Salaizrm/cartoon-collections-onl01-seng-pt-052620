c47626221eed3ca49db5daf23d7bafda0da060e7
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-10 01:01:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,9 @@ end
 
 def summon_captain_planet(array)
   array.map do |element|
-    element.capitalize
+    element.split(", ")
   end
-  array
+  array.join("!")
 end
 
 def long_planeteer_calls# code an argument here
