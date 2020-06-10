a49d0a36d113da09bf8d5ef30d2052406c6c47d4
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-10 01:00:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |element|
+    element.capitalize
+  end
+  array
 end
 
 def long_planeteer_calls# code an argument here
